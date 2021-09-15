9667a848ba10b923734ebe4c43ccd7c5fd057c9d
landrea-velez <landrea.velez@gmail.com>
2021-09-15 09:55:32 -0500

    [ADD] Services added

--- src/startup/container.js
+++ src/startup/container.js
@@ -5,7 +5,12 @@ const config = require("../config");
 const app = require(".");
 
 //services
-const { HomeService } = require("../services");
+const {
+  HomeService,
+  UserService,
+  IdeaService,
+  CommentService,
+} = require("../services");
 
 //controllers
 const { HomeController } = require("../controllers");
@@ -34,6 +39,9 @@ container
   })
   .register({
     HomeService: asClass(HomeService).singleton(),
+    UserService: asClass(UserService).singleton(),
+    CommentService: asClass(CommentService).singleton(),
+    IdeaService: asClass(IdeaService).singleton(),
   })
   .register({
     HomeController: asClass(HomeController.bind(HomeController)).singleton(),
