8271476e63b45d63cca5f84db12bab129aba702d
landrea-velez <landrea.velez@gmail.com>
2021-09-14 20:47:35 -0500

    [ADD] useUnifiedTopology added to mongoose config

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const { MONGO_URI } = container.resolve("config");
 
 const mongoose = require("mongoose");
 mongoose.set("useCreateIndex", true);
+
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
   .then(() => server.start())
   .catch(console.log);
