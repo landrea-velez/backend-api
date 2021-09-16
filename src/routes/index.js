09db0200a74e81a742fe4a73437261eb5233da9b
landrea-velez <landrea.velez@gmail.com>
2021-09-16 15:03:20 -0500

    [ADD] Routes Added

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,20 +4,27 @@ const helmet = require("helmet");
 const compression = require("compression");
 require("express-async-errors");
 const { NotFoundMiddleware, ErrorMiddleware } = require("../middlewares");
-const notFoundMiddleware = require("../middlewares/not-found.middleware");
-const errorMiddleware = require("../middlewares/error.middleware");
 
-module.exports = function ({ HomeRoutes }) {
+module.exports = function ({
+  HomeRoutes,
+  UserRoutes,
+  IdeaRoutes,
+  CommentRoutes,
+}) {
   const router = express.Router();
   const apiRoutes = express.Router();
 
   apiRoutes.use(express.json()).use(cors()).use(helmet()).use(compression());
 
   apiRoutes.use("/home", HomeRoutes);
+  apiRoutes.use("/user", UserRoutes);
+  apiRoutes.use("/idea", IdeaRoutes);
+  apiRoutes.use("/comment", CommentRoutes);
+
   router.use("/v1/api", apiRoutes);
 
-  router.use(notFoundMiddleware);
-  router.use(errorMiddleware);
+  router.use(NotFoundMiddleware);
+  router.use(ErrorMiddleware);
 
   return router;
 };
