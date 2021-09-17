0055810e78244a59f189d489ecfbda867d3692cc
landrea-velez <landrea.velez@gmail.com>
2021-09-17 10:53:04 -0500

    [ADD] Cache added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,26 @@
 const { Router } = require("express");
-const { AuthMiddleware, ParseIntMiddleware } = require("../middlewares");
+const {
+  AuthMiddleware,
+  ParseIntMiddleware,
+  CacheMiddleware,
+} = require("../middlewares");
+const { CACHE_TIME } = require("../helpers");
 
 module.exports = function ({ UserController }) {
   const router = Router();
 
-  router.get("", [AuthMiddleware, ParseIntMiddleware], UserController.getAll);
+  router.get(
+    "",
+    [
+      ParseIntMiddleware,
+      ParseIntMiddleware,
+      CacheMiddleware(CACHE_TIME.ONE_HOUR),
+    ],
+    UserController.getAll
+  );
   router.get("/:userId", UserController.get);
-  router.patch("/:userId", UserController.update);
-  router.delete("/:userId", UserController.delete);
+  router.patch("/:userId", AuthMiddleware, UserController.update);
+  router.delete("/:userId", AuthMiddleware, UserController.delete);
 
   return router;
 };
